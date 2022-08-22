27f0c4929ef3d65725b4dcbac76fb8b464b420d1
TheLordSide <cedricaguigah@gmail.com>
2022-08-22 12:32:25 +0200

    fixing cours array

--- cours/cours.php
+++ cours/cours.php
@@ -6,10 +6,11 @@ function cours(){
     global $pdo;
     $query = $pdo->prepare("select * from cours");
     $query->execute();
-    $response["total"]=$query->rowCount();
-    $response["Liste"]=$query->fetchAll();
     $response["success"]=true;
     $response["message"]="liste des cours disponibles";
+    $response["total"]=$query->rowCount();
+    $response["liste"]=$query->fetchAll();
+   
     echo json_encode($response);
 }
 
