c93db6136dd0bdbfedfb260c147edc232febb851
TheLordSide <cedricaguigah@gmail.com>
2022-08-31 11:43:59 +0200

    fixing cours array

--- cours/cours.php
+++ cours/cours.php
@@ -6,9 +6,6 @@ function cours(){
     global $pdo;
     $query = $pdo->prepare("select * from cours");
     $query->execute();
-    $response["success"]=true;
-    $response["message"]="liste des cours disponibles";
-    $response["total"]=$query->rowCount();
     $response["liste"]=$query->fetchAll();
    
     echo json_encode($response);
--- cours/createcours.php
+++ cours/createcours.php
@@ -1,11 +1,46 @@
 <?php
 require_once("../config/api.php");
 include_once("../config/json-header.php");
-$pdo= getconnexion();
-function createcours(){
+$pdo = getconnexion();
+function createcours()
+{
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (
+            !empty($_POST['cours_nom']) || !empty($_POST['cours_auteur']) || !empty($_POST['cours_module']) ||
+            !empty($_POST['cours_image']) || !empty($_POST['cours_description']) || !empty($_POST['cours_prix']) ||
+            !empty($_POST['cours_prix2']) ||
+            !empty($_POST['cours_volumeh'])
+        ) {
+            coursexit(
+                $_POST['cours_nom'],
+                $_POST['cours_auteur'],
+                $_POST['cours_module'],
+                $_POST['cours_image'],
+                $_POST['cours_description'],
+                $_POST['cours_prix'],
+                $_POST['cours_prix2'],
+                $_POST['cours_volumeh']
+            );
+        } else {
+            resultjson(false, "Plusieurs informations manquantes");
+        }
+    }
+}
+
+function coursexit(
+    $coursnom,
+    $coursauteur,
+    $coursmodule,
+    $coursimage,
+    $coursdescription,
+    $coursprix,
+    $coursprix2,
+    $coursvolumeh
+) {
+    global $pdo;
 
 }
 
-function coursexit(){
+function generatecoursserial(){
     
 }
